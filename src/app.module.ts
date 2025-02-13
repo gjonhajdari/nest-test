a02b54fc6a8e063041384df583a007a4953afada
chon <gjon.hajdari@student.uni-pr.edu>
2025-02-13 16:14:55 +0100

    Update formatting using biome.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './api/users/users.module';
-import { ReportsModule } from './api/reports/reports.module';
+import { Module } from "@nestjs/common";
+import { ReportsModule } from "./api/reports/reports.module";
+import { UsersModule } from "./api/users/users.module";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [UsersModule, ReportsModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [UsersModule, ReportsModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
