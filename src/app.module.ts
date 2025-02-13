3017f2613196d77f9a943da91f27eebeef6f74bf
chon <gjon.hajdari@student.uni-pr.edu>
2025-02-13 16:29:09 +0100

    Create connection with TypeORM using sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { ReportsModule } from "./api/reports/reports.module";
+import { User } from "./api/users/user.entity";
 import { UsersModule } from "./api/users/users.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-	imports: [UsersModule, ReportsModule],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: "sqlite",
+			database: "db.sqlite",
+			entities: [User],
+			synchronize: true,
+		}),
+		UsersModule,
+		ReportsModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
